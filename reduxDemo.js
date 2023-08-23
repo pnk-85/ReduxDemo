0f559cd1c5d09b15e64e2e53b26b20f52290038a
pnk-85 <pnkghogare27@gmail.com>
2023-08-23 11:09:20 +0530

    increment and decrement by 5

--- reduxDemo.js
+++ reduxDemo.js
@@ -4,12 +4,12 @@ const redux = require('redux');
 const couterReducer = (state = {counter : 0}, action) => {
     if(action.type === 'increment'){
         return {
-            counter : state.counter + 1
+            counter : state.counter + 5
         };
     };
     if(action.type === 'decrement'){
         return { 
-            counter : state.counter - 1
+            counter : state.counter - 5
         };
     };
 };
