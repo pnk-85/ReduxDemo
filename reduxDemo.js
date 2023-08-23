1a22c296362bb2304fcd5c2818c334cce6f140e8
pnk-85 <pnkghogare27@gmail.com>
2023-08-23 11:00:26 +0530

    create redux demo

---
+++ reduxDemo.js
@@ -0,0 +1,32 @@
+const redux = require('redux');
+ 
+
+const couterReducer = (state = {counter : 0}, action) => {
+    if(action.type === 'increment'){
+        return {
+            counter : state.counter + 1
+        };
+    };
+    if(action.type === 'decrement'){
+        return { 
+            counter : state.counter - 1
+        };
+    };
+};
+
+const store = redux.createStore(couterReducer);
+
+
+const conterSubScriber = () => {
+    const latestState = store.getState();
+    console.log(latestState);
+};
+
+
+store.subscribe(conterSubScriber);
+
+store.dispatch({type : 'increment'});
+store.dispatch({type : 'increment'});
+store.dispatch({type : 'increment'});
+store.dispatch({type : 'decrement'});
+store.dispatch({type : 'decrement'});
